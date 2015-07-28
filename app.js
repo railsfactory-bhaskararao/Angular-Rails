283e0cc1a9f2251ea1bef73ab4237342028a364e
bhaskararao <bhaskararao@railsfactory.org>
2015-07-28 15:58:28 +0530

    Upvote existing posts

--- app.js
+++ app.js
@@ -15,4 +15,7 @@ function($scope){
     $scope.posts.push({title: $scope.title, upvotes: 4})
     $scope.title = '';
   };
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
 }]);
